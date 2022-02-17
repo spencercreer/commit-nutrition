7c1d5536f7fc8ade9e6b415088098d1138ac526b
spencercreer <spencercreer@gmail.com>
2022-02-17 01:56:38 -0700

    added nav component

--- client/src/App.js
+++ client/src/App.js
@@ -1,3 +1,4 @@
+import Header from './components/Header'
 import FoodForm from './components/FoodForm';
 import { Container } from 'react-bootstrap'
 import './App.css';
@@ -5,8 +6,8 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <Container>
-        <div>COMMIT Nutrition</div>
+      <Header />
+      <Container className="my-3">
         <FoodForm />
       </Container>
     </div>
