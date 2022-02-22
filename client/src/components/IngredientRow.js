4a5d8aba78ad8118c18eba8902f7ca7f8cffe196
spencercreer <spencercreer@gmail.com>
2022-02-21 17:31:25 -0700

    added servings and size

--- client/src/components/IngredientRow.js
+++ client/src/components/IngredientRow.js
@@ -1,25 +1,35 @@
-import { useState } from 'react'
-import { Select, InputNumber, Row, Col } from 'antd'
+import { useState, useEffect } from 'react'
+import { Select, InputNumber, Row, Col, Input } from 'antd'
 
 const { Option } = Select
 
 const IngredientRow = ({ foods }) => {
+    const [food, setFood] = useState()
+    const [servings, setServings] = useState(1)
 
-    const [foodData, setFoodData] = useState()
-    
-    function onChange(value) {
-        console.log(`selected ${value}`)
+    function onFoodChange(value) {
+        setFood(foods[parseInt(value.key)])
     }
+
+    function onServingChange(value) {
+        setServings(value)
+    }
+
+    const carbs = food?.carbs * servings
+    const protein = food?.protein * servings
+    const fat = food?.fat * servings
+    const sodium = food?.sodium * servings
 
     return (
         <Row>
-            <Col sm={6}>
+            <Col xs={18}>
                 <Select
-                    style={{ width: '150px' }}
+                    style={{ width: '300px' }}
                     showSearch
                     placeholder="Food"
                     optionFilterProp="children"
-                    onChange={onChange}
+                    labelInValue
+                    onChange={onFoodChange}
                     filterOption={(input, option) =>
                         option.children.toLowerCase().indexOf(input.toLowerCase()) >= 0
                     }
@@ -30,38 +40,66 @@ const IngredientRow = ({ foods }) => {
                         ))
                     }
                 </Select>
-            </Col>
-            <Col sm={4}>
+                <Select
+                    style={{ width: '150px' }}
+                    showSearch
+                    placeholder="Serving Size"
+                    optionFilterProp="children"
+                    labelInValue
+                    // onChange={onChange}
+                    filterOption={(input, option) =>
+                        option.children.toLowerCase().indexOf(input.toLowerCase()) >= 0
+                    }
+                >
+                    {
+                        foods?.map((food, i) => (
+                            <Option key={i} value={food?.name}>{food?.name}</Option>
+                        ))
+                    }
+                </Select>
                 <InputNumber
-                    style={{ marginRight: '5px' }}
-                    addonAfter="g"
-                    step="0.01"
-                    disabled
+                    style={{ width: '150px' }}
+                    placeholder="Number of Servings"
+                    defaultValue={1}
+                    onChange={onServingChange}
+                    
                 />
             </Col>
-            <Col sm={4}>
-                <InputNumber
-                    style={{ marginRight: '5px' }}
-                    addonAfter="g"
-                    step="0.01"
-                    disabled
-                />
-            </Col>
-            <Col sm={4}>
-                <InputNumber
-                    style={{ marginRight: '5px' }}
-                    addonAfter="g"
-                    step="0.01"
-                    disabled
-                />
-            </Col>
-            <Col sm={4}>
-                <InputNumber
-                    style={{ marginRight: '5px' }}
-                    addonAfter="mg"
-                    step="0.01"
-                    disabled
-                />
+            <Col xs={6}>
+                <Row>
+                    <Col md={6}>
+                        <InputNumber
+                            style={{ marginRight: '5px' }}
+                            addonAfter="g"
+                            value={carbs}
+                            disabled
+                        />
+                    </Col>
+                    <Col md={6}>
+                        <InputNumber
+                            style={{ marginRight: '5px' }}
+                            addonAfter="g"
+                            value={protein}
+                            disabled
+                        />
+                    </Col>
+                    <Col md={6}>
+                        <InputNumber
+                            style={{ marginRight: '5px' }}
+                            addonAfter="g"
+                            value={fat}
+                            disabled
+                        />
+                    </Col>
+                    <Col md={6}>
+                        <InputNumber
+                            style={{ marginRight: '5px' }}
+                            addonAfter="mg"
+                            value={food?.sodium}
+                            disabled
+                        />
+                    </Col>
+                </Row>
             </Col>
         </Row>
     )
