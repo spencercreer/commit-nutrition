0103f1c526e4b6587c36573d05a1fd868659955e
spencercreer <spencercreer@gmail.com>
2022-05-25 06:26:52 -0700

    added production conditional in server

--- server/server.js
+++ server/server.js
@@ -1,12 +1,18 @@
 const express = require('express');
+const path = require('path');
 const db = require('./config/connection');
 const routes = require('./routes');
 
-const PORT = process.env.PORT || 3001;
 const app = express();
+const PORT = process.env.PORT || 3001;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.join(__dirname, '../client/build')));
+}
+
 app.use(routes);
 
 db.once('open', () => {
