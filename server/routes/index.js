eaf6dd39c9710aef254aa6112868c1c0fe0fed4c
spencercreer <spencercreer@gmail.com>
2022-05-26 18:56:49 -0700

    fixed route issue on prod

--- server/routes/index.js
+++ server/routes/index.js
@@ -3,8 +3,9 @@ const apiRoutes = require('./api')
 
 router.use('/api', apiRoutes)
 
-// router.use((req, res) => {
-//     return res.send('Wrong route')
-// })
+// serve up react front-end in production
+router.use((req, res) => {
+    res.sendFile(path.join(__dirname, '../../client/build/index.html'));
+});
 
 module.exports = router
