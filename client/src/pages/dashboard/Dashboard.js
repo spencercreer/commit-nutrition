1b3412b1cc8a94b5903286db567a1f673cb47a72
spencercreer <spencercreer@gmail.com>
2022-07-03 00:33:07 -0700

    loading dashboard

--- client/src/pages/dashboard/Dashboard.js
+++ client/src/pages/dashboard/Dashboard.js
@@ -13,14 +13,28 @@ const { Content } = Layout
 const Dashboard = () => {
   const { data: mealData, loading } = useGet('/api/meals/today')
   // console.log(mealData)
+  // loading = true
   return (
     <>
       {
         loading ?
-          <>
-            <Skeleton.Input active={true} size={'large'} />
-            <LoadingCards number={12} />
-          </>
+          <Content>
+            <Row style={{ padding: '5px' }}>
+              <Col xs={24}>
+                <Card>
+                  <Skeleton.Input active={true} size={'large'} />
+                </Card>
+              </Col>
+            </Row>
+            <Row>
+              <Col xs={24} md={14} style={{ padding: '5px' }}>
+                <LoadingCards number={1} />
+              </Col>
+              <Col xs={24} md={10} style={{ padding: '5px' }}>
+                <LoadingCards number={1} />
+              </Col>
+            </Row>
+          </Content>
           :
           <Content style={{ margin: '10px 60px' }}>
             <Row style={{ padding: '5px' }}>
