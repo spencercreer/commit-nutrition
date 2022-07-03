503148ceeecdb8b9716de8c0d00af720a3b9d562
spencercreer <spencercreer@gmail.com>
2022-07-03 00:09:46 -0700

    style dashboard

--- client/src/pages/dashboard/Dashboard.js
+++ client/src/pages/dashboard/Dashboard.js
@@ -1,12 +1,14 @@
+// Components
 import MealCard from "../../components/MealCard"
 import ProfileForm from "./components/ProfileForm"
+import NutrientsChart from "../../components/NutrientsChart"
 import LoadingCards from "../../components/LoadingCards"
 // Antd
-import { Layout, Skeleton } from 'antd'
+import { Layout, Row, Col, Card, Skeleton } from 'antd'
 // Utils
 import { useGet } from '../../utils/API'
 
-const { Content, Sider } = Layout
+const { Content } = Layout
 
 const Dashboard = () => {
   const { data: mealData, loading } = useGet('/api/meals/today')
@@ -21,9 +23,24 @@ const Dashboard = () => {
           </>
           :
           <Content style={{ margin: '10px 60px' }}>
-            <h1>Welcome Spencer</h1>
-            <h2>Today's Meal Plan</h2>
-            <MealCard meal={mealData} />
+            <Row style={{ padding: '5px' }}>
+              <Col xs={24}>
+                <Card>
+                  <h1>Welcome Spencer</h1>
+                </Card>
+              </Col>
+            </Row>
+            <Row>
+              <Col xs={24} md={14} style={{ padding: '5px' }}>
+                {/* <h2>Today's Meal Plan</h2> */}
+                <MealCard meal={mealData} />
+              </Col>
+              <Col xs={24} md={10} style={{ padding: '5px' }}>
+                <NutrientsChart
+                  nutrients={{ calories: mealData?.calories, carbs: mealData?.carbs, protein: mealData?.protein, fat: mealData?.fat, sodium: mealData?.sodium }}
+                />
+              </Col>
+            </Row>
             <ProfileForm />
           </Content>
       }
