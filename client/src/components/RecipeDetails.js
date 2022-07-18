fe3ca1fff67aec0cfed65cf7405f51faf5b597e3
spencercreer <spencercreer@gmail.com>
2022-07-17 18:26:17 -0700

    implemented fraction.js for recipe details modal

--- client/src/components/RecipeDetails.js
+++ client/src/components/RecipeDetails.js
@@ -1,12 +1,19 @@
 import NutrientsRow from "./NutrientsRow"
 import NutrientsChart from "./NutrientsChart"
+import Fraction from "fraction.js"
 
 const RecipeDetails = ({ recipeData }) => {
+
+  const calculateAmount = (food) => {
+    const amount = new Fraction(food?.foodId?.serving.size * food?.number_of_servings)
+    return amount.toFraction()
+  }
+
   return (
     <>
       {
         recipeData.ingredients.map((food, i) => (
-          <div key={i}>{(food?.foodId?.serving.size * food?.number_of_servings).toFixed(3)} {food?.foodId?.serving.unit} {food?.foodId?.name}</div>
+          <div key={i}>{calculateAmount(food)} {food?.foodId?.serving.unit} {food?.foodId?.name}</div>
         ))
       }
       <div style={{ marginTop: '10px', marginBottom: '5px' }}>
--- client/src/components/ServingDetails.js
+++ client/src/components/ServingDetails.js
@@ -1,12 +1,19 @@
 import NutrientsRow from "./NutrientsRow"
 import NutrientsChart from "./NutrientsChart"
+import Fraction from "fraction.js"
 
 const ServingDetails = ({ recipeData }) => {
+
+    const calculateAmount = (food) => {
+        const amount = new Fraction(food?.foodId?.serving.size * food?.number_of_servings / recipeData?.recipe_servings)
+        return amount.toFraction()
+    }
+
     return (
         <>
             {
                 recipeData.ingredients.map((food, i) => (
-                    <div key={i}>{(food?.foodId?.serving.size * food?.number_of_servings / recipeData?.recipe_servings).toFixed(3)} {food?.foodId?.serving.unit} {food?.foodId?.name}</div>
+                    <div key={i}>{calculateAmount(food)} {food?.foodId?.serving.unit} {food?.foodId?.name}</div>
                 ))
             }
             <div style={{ marginTop: '10px', marginBottom: '5px' }}>
